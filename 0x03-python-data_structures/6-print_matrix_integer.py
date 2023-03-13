9b786f77b2e8941b7410704d82e524636ede5cd5
nevillechemwa <nevillechemwa@gmail.com>
2023-03-13 10:00:26 -0700

    function that prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,39 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-        for i in matrix:
-            for j in i:
-                if j != i[-1]:
-                    print('{:d}'.format(j), end=" ")
-                else:
-                    print('{:d}'.format(j), end="")
-            print()
+def is_palindrome(head):
+    """
+    Check if a singly linked list is a palindrome.
+    :param head: Head node of the linked list
+    :return: True if the linked list is a palindrome, False otherwise
+    """
+    if not head:
+        return True
+
+    # Get the length of the linked list
+    length = 0
+    current = head
+    while current:
+        length += 1
+        current = current.next
+
+    # Reverse the first half of the linked list
+    mid = length // 2
+    prev = None
+    current = head
+    for i in range(mid):
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    # If the length of the linked list is odd, skip the middle node
+    if length % 2 != 0:
+        current = current.next
+
+    # Compare the reversed first half with the second half
+    while prev and current:
+        if prev.val != current.val:
+            return False
+        prev = prev.next
+        current = current.next
+
+    return True
+
